8a7389ff8e09a96dca9e51a40789d6d4faf153ba
Dominic Fernandez <dominic.fernandez1023@gmail.com>
2020-10-15 15:50:19 -0700

    major style updates for goalBuilder page

--- src/pages/GoalBuilder.js
+++ src/pages/GoalBuilder.js
@@ -8,19 +8,90 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import CardDeck from 'react-bootstrap/CardDeck';
+import CardColumns from 'react-bootstrap/CardColumns';
 import { useParams } from 'react-router';
 import Dropdown from 'react-bootstrap/Dropdown';
 import Spinner from 'react-bootstrap/Spinner';
 import Badge from 'react-bootstrap/Badge';
 import ProgressBar from 'react-bootstrap/ProgressBar';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 function GoalBuilder(props) {
 	
 	const ID = props.match.params.goalId;
 	const GOALOPTIONS = [
 		{value: "edit", displayName: "Edit"},
+		{value: "details", displayName: "Details"},
 		{value: "share", displayName: "Share"},
 		{value: "delete", displayName: "Delete"}
+	];
+	const testTasks = [
+		{
+			name: "Task 1", 
+			deadline: "10/20/20", 
+			description: "testing task 1", 
+			percentageCompleted: 5,
+			subTasks: [
+				{name: "subTask1", description: "yooooooo this is a subtask"}, 
+				{name: "subTask2", description: "yooooooo this is a subtask"}, 
+				{name: "subTask3", description: "yooooooo this is a subtask"}
+			]
+		},
+		{
+			name: "Task 2", 
+			deadline: "10/18/20", 
+			description: "testing task 2 yoooooo", 
+			percentageCompleted: 0,
+			subTasks: [
+				{name: "subTask1", description: "yooooooo this is a subtask"}, 
+				{name: "subTask2", description: "yooooooo this is a subtask"}, 
+				{name: "subTask3", description: "yooooooo this is a subtask"}
+			]
+		},
+		{
+			name: "Task 3", 
+			deadline: "10/21/20", 
+			description: "testing task 3 dawggggggg", 
+			percentageCompleted: 0,
+			subTasks: [
+				{name: "subTask1", description: "yooooooo this is a subtask"}, 
+				{name: "subTask2", description: "yooooooo this is a subtask"}, 
+				{name: "subTask3", description: "yooooooo this is a subtask"}
+			]
+		},
+		{
+			name: "Task 4", 
+			deadline: "10/20/20", 
+			description: "testing task 4", 
+			percentageCompleted: 20,
+			subTasks: [
+				{name: "subTask1", description: "yooooooo this is a subtask"}, 
+				{name: "subTask2", description: "yooooooo this is a subtask"}, 
+				{name: "subTask3", description: "yooooooo this is a subtask"}
+			]
+		},
+		{
+			name: "Task 5", 
+			deadline: "10/18/20", 
+			description: "testing task 5 yoooooo", 
+			percentageCompleted: 75,
+			subTasks: [
+				{name: "subTask1", description: "yooooooo this is a subtask"}, 
+				{name: "subTask2", description: "yooooooo this is a subtask"}, 
+				{name: "subTask3", description: "yooooooo this is a subtask"}
+			]
+		},
+		{
+			name: "Task 6", 
+			deadline: "10/21/20", 
+			description: "testing task 6 dawggggggg", 
+			percentageCompleted: 100,
+			subTasks: [
+				{name: "subTask1", description: "yooooooo this is a subtask"}, 
+				{name: "subTask2", description: "yooooooo this is a subtask"}, 
+				{name: "subTask3", description: "yooooooo this is a subtask"}
+			]
+		}
 	];
 	
 	const[goal, setGoal] = useState();
@@ -58,7 +129,7 @@ function GoalBuilder(props) {
 			<Container fluid>
 				<Row>
 					<Col>
-						<Card>
+						<Card style = {{border: "none"}}>
 							<Card.Header>
 								<Row>
 									<Col>
@@ -76,7 +147,6 @@ function GoalBuilder(props) {
 									<Col style = {{textAlign: "right"}}>
 										<Dropdown style = {{marginLeft: "1%"}}>
 											<Dropdown.Toggle className = "my-dropdown-toggle" variant = "light">
-												&#8942;
 											</Dropdown.Toggle>
 											<Dropdown.Menu>
 											{GOALOPTIONS.map((option) => {
@@ -90,7 +160,116 @@ function GoalBuilder(props) {
 								</Row>
 							</Card.Header>
 							<Card.Body>
-								
+								<Row>
+									<Col md = {5}>
+										<Row>
+											<Col xs = {1}>
+												💬
+											</Col>
+											<Col xs = {11}>
+												<p>
+													<i> {goal.description} </i>
+												</p>
+											</Col>
+										</Row>
+										<Row>
+											<Col xs = {1}>
+												👤
+											</Col>
+											<Col xs = {11}>
+												<p> {goal.userCreated} </p>
+											</Col>
+										</Row>
+										<Row>
+											<Col xs = {1}>
+												❤️
+											</Col>
+											<Col xs = {11}>
+												<ProgressBar 
+													now = {100-goal.percentageCompleted} 
+													label = {(100-goal.percentageCompleted).toString() + "%"} 
+													variant = "danger"
+													style = {{height: "30px"}}
+												/>
+											</Col>
+										</Row>
+									</Col>
+									<Col md= {7} style = {{textAlign: "center"}}>
+										<h5> *goal analytics section* </h5>
+									</Col>
+								</Row>
+								<br/>
+								<Row>
+									<Col>
+										<h5> 📝 Tasks </h5>
+									</Col>
+								</Row>
+
+								<Row>
+									<Col>
+										<Row>
+											{testTasks.map((task) => {
+												return (
+													<Col md = {3}>
+														<Card style = {{marginBottom: "5%"}}>
+															<Card.Header> 
+																<Row>
+																	<Col sm = {3}>
+																		{task.name}
+																	</Col>
+																	<Col sm = {3}>
+																		<Badge pills variant = "dark"> Due: {task.deadline} </Badge>
+																	</Col>
+																	<Col sm = {6} style = {{textAlign: "right"}}>
+																		<Dropdown>
+																			<Dropdown.Toggle className = "my-dropdown-toggle" variant = "light">
+																			</Dropdown.Toggle>
+																			<Dropdown.Menu>
+																			{GOALOPTIONS.map((option) => {
+																				return (
+																					<Dropdown.Item> {option.displayName} </Dropdown.Item>
+																				);
+																			})}
+																			</Dropdown.Menu>
+																		</Dropdown>					
+																	</Col>
+																</Row>
+																<Row>
+																	<Col>
+																		{task.percentageCompleted}% completed
+																	</Col>
+																</Row>
+																<Row>
+																	<Col>
+																		<div style = {{whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis"}}>
+																			<i> {task.description} </i>
+																		</div>
+																	</Col>
+																</Row>
+															</Card.Header>
+															<ListGroup variant = "flush">
+																{task.subTasks.map((sub) => {
+																	return (
+																		<ListGroup.Item>
+																			<Row>
+																				<Col>
+																					<Form.Check
+																						type = "checkbox"
+																						label = {sub.name}
+																					/>
+																				</Col>
+																			</Row>
+																		</ListGroup.Item>
+																	);
+																})}
+															</ListGroup>
+														</Card>
+													</Col>
+												);
+											})}
+										</Row>
+									</Col>
+								</Row>
 							</Card.Body>
 						</Card>
 					</Col>
